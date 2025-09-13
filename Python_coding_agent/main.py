b8d7c898bd51735f6c556561c92039e21be241fc
Dimitar Pashev <dim.pashev@gmail.com>
2025-09-13 12:57:08 +0300

    make api call

--- Python_coding_agent/main.py
+++ Python_coding_agent/main.py
@@ -1,6 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from python-coding-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents="Why is the sky blue?"
+    )
+
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("No response")
+        return
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
 
 
-if __name__ == "__main__":
-    main()
+main()
