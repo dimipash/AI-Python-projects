1d035309500b04d46c52daf308e6c258cab38f8c
Dimitar Pashev <dim.pashev@gmail.com>
2025-09-14 13:13:21 +0300

    add conversation history

--- Python_coding_agent/main.py
+++ Python_coding_agent/main.py
@@ -1,15 +1,28 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    if len(sys.argv) < 2:
+        print("I need a prompt!")
+        sys.exit(1)
+        
+    prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents="Why is the sky blue?"
-    )
+        model="gemini-2.0-flash-001", contents=messages)
 
     print(response.text)
     if response is None or response.usage_metadata is None:
@@ -18,7 +31,6 @@ def main():
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
 
 
 main()
