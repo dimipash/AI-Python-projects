441f02cd578660f5dfaf4ee74e1b90af289ed413
Dimitar Pashev <dim.pashev@gmail.com>
2025-09-14 13:20:13 +0300

    add verbose flag for debugging

--- Python_coding_agent/main.py
+++ Python_coding_agent/main.py
@@ -5,7 +5,6 @@ from google import genai
 from google.genai import types
 
 
-
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,23 +13,28 @@ def main():
     if len(sys.argv) < 2:
         print("I need a prompt!")
         sys.exit(1)
-        
+
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+
     prompt = sys.argv[1]
 
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=prompt)])
-    ]
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=messages)
+        model="gemini-2.0-flash-001", contents=messages
+    )
 
     print(response.text)
     if response is None or response.usage_metadata is None:
         print("No response")
         return
-
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 main()
