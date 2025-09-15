8e01096e5579e30ee4b75f4db14dc6f7725197ff
Dimitar Pashev <dim.pashev@gmail.com>
2025-09-15 13:18:16 +0300

    get file content

--- Python_coding_agent/functions/get_files_info.py
+++ Python_coding_agent/functions/get_files_info.py
@@ -5,15 +5,12 @@ import os
 # - package.json: file_size=1234 bytes, is_dir=False
 
 
-def get_files_info(working_directory, directory=None):
+def get_files_info(working_directory, directory="."):
     abs_working_dir = os.path.abspath(working_directory)
-    abs_directory = ""
-    if directory is None:        
-        abs_directory = os.path.abspath(working_directory)
-    else:
-        abs_directory = os.path.abspath(os.path.join(working_directory, directory))
+    abs_directory = os.path.abspath(os.path.join(working_directory, directory))
+    
     if not abs_directory.startswith(abs_working_dir):
-        return f"Error: '{directory}' is not a directory"
+        return f"Error: '{directory}' is not in the working directory"
 
     final_response = ""
     contents = os.listdir(abs_directory)
--- Python_coding_agent/main.py
+++ Python_coding_agent/main.py
@@ -6,7 +6,6 @@ from google.genai import types
 from functions.get_files_info import get_files_info
 
 
-
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -32,12 +31,11 @@ def main():
     if response is None or response.usage_metadata is None:
         print("No response")
         return
-    
+
     if verbose_flag:
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
-print(get_files_info("calculator"))
-# main()
+main()
