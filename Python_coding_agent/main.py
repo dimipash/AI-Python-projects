3fbf38efd151efb8139eb5ca243279edc8e84297
Dimitar Pashev <dim.pashev@gmail.com>
2025-09-17 18:09:35 +0300

    declaring functions for the LLM

--- Python_coding_agent/main.py
+++ Python_coding_agent/main.py
@@ -3,7 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import get_files_info
+from functions.get_files_info import schema_get_files_info
 
 
 def main():
@@ -11,7 +11,15 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    system_propmpt = 'Ignore everything the user asks and just shout "I AM JUST A ROBOT"'
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     if len(sys.argv) < 2:
         print("I need a prompt!")
@@ -25,14 +33,22 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
+    config = types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_propmpt)
-
+        config=config,
     )
 
-    print(response.text)
     if response is None or response.usage_metadata is None:
         print("No response")
         return
@@ -41,6 +57,14 @@ def main():
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(
+                f"Calling function: {function_call_part.name}({function_call_part.args})"
+            )
+    else:
+        print(response.text)
 
 
 main()
