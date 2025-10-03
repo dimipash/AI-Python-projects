3f37d3ea60cd2eae57f21e6a5a0d3cc41561e180
Dimitar Pashev <dim.pashev@gmail.com>
2025-10-03 16:10:15 +0300

    add Postgres db to Django

--- Reddit_scraping_pipeline/src/dlphome/settings.py
+++ Reddit_scraping_pipeline/src/dlphome/settings.py
@@ -74,6 +74,18 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.environ.get("DATABASE_URL") or ""
+if DATABASE_URL and DATABASE_URL != "":
+    import dj_database_url
+
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=300,
+            conn_health_checks=True,
+        )
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
